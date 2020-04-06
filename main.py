3e5c9797b6143d45b0c3b8dcf47551de5cf2a6e3
wegoerling <11wgoerling@gmail.com>
2020-04-06 15:43:19 -0400

    Second Commit

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'curd.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Fraunhofer!'
+
+
 
